fd7cc40765704e312f1fb0586cb7d88106216e8c
dominikus1993 <dominikus1910@gmail.com>
2019-09-29 22:54:55 +0200

    Jwt

--- src/Identity.Infrastructure/Token.fs
+++ src/Identity.Infrastructure/Token.fs
@@ -1,12 +1,18 @@
 namespace Identity.Infrastructure
 open Identity.Domain.UseCases
 open System.Threading.Tasks
+open System.Security.Claims
+open System.IdentityModel.Tokens.Jwt
 
 module Token = 
     open Identity.Domain
     open Identity.Domain.Model
+    open System
 
     type JwtConfig = { expiryHours: int64; secretKey: string; issuer: string; audience: string }
    
     let generateJwt(config: JwtConfig)(user: TokenGeneratorParams) : UserToken = 
-        failwith "chgadsjhgdsah"
+        let now = DateTime.UtcNow
+        let expiry = now.AddHours(config.expiryHours |> float)
+        let claims = [| Claim(JwtRegisteredClaimNames.Sub, user.userId); Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()) |]
+        failwith "daasd"
